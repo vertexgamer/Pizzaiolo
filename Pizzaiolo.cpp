43cf55bc3e235ca0a44e19ac47ed9bc25dc22420
vertexgamer <31879979+vertexgamer@users.noreply.github.com>
2022-05-03 18:32:54 +0200

    Update Pizzaiolo.cpp

--- Pizzaiolo.cpp
+++ Pizzaiolo.cpp
@@ -24,6 +24,9 @@ int i;
 int loop = 0;
 string cartellaoutput;
 int z;
+string spazifix = "\"\" ";
+string inizio;
+string iniziocartellaoutput;
 
 int main()
 {
@@ -31,7 +34,7 @@ int main()
     {
         if (loop == 0)
         {
-            cout << "Path dell eseguibile senza spazi / Executable path without spaces" << endl;  //input path of the .exe
+            cout << "Percorso dell eseguibile / Executable path" << endl;  //input path of the .exe
             getline(std::cin, eseguibile);
         }
         cout << "Scrivi gli argomenti ma sostituisci con il nome \"perin\" e \"perout\" il percorso di input e output / Write the arguments but replace the input and output path with the word \"perin\" and \"perout\"" << endl; //the user write the desired arguments but replace the input and output path with "percorso"
@@ -39,9 +42,9 @@ int main()
             cin.ignore();
         }
         getline(std::cin, argomenti);
-        cout << "Trascina la cartella con i files (il path non deve avere spazi) / Drag the folder containing the files (the path mustn't have spaces)" << endl; //the user drag the folder which contains the files to be encoded
+        cout << "Trascina la cartella con i files (se il percorso contiene spazi, dovrai aggiungere \" alla fine di \"perout\" + l'estensione del file) / Drag the folder containing the files(if the path contain spaces, add \" after \"perout\" + the file extension)" << endl; //the user drag the folder which contains the files to be encoded
         getline(std::cin, cartella);
-        cout << "Desideri che l'output vada su una cartella specifica? Se no, lascia il campo vuoto" << endl;
+        cout << "Desideri che l'output vada su una cartella specifica? Se no, lascia il campo vuoto. (se il percorso ha spazi, leggi sopra) / Do you desire a specific output folder? If not, leave blank. (if the path have spaces, read above)" << endl;
         getline(std::cin, cartellaoutput);
         cout << "Inserisci quante istanze saranno attive simultaneamente (USA UN VALORE MAGGIORE DI 0) / Insert the number of instances allowed at the same time (THE VALUE MUST BE GREATER THAN 0)" << endl;
         cin >> operai;   //number of simultaneus instances
@@ -50,13 +53,27 @@ int main()
         i = eseguibile.find_last_of("\\");          //the executable name is extracted from the path given before
         nomeeseguibile = eseguibile.substr(i + 1);
 
+        string inizio(cartella, 0, 1);
+        string iniziocartellaoutput(cartellaoutput, 0, 1);
+
+        if (inizio == "\"") {
+            cartella.erase(0, 1);
+            cartella.pop_back();
+        }
+        if (iniziocartellaoutput == "\"")
+        {
+            cartellaoutput.pop_back();
+        }
         for (const auto& entry : fs::recursive_directory_iterator(cartella)) {    //it gets the path of the files inside the folder
             percorso = entry.path().string();
+            if (inizio == "\"") {
+                percorso = "\"" + percorso + "\"";
+            }
 
             while (argomenti.find("perin") != string::npos)
                 argomenti.replace(argomenti.find("perin"), 5, percorso);    //it replace "percorso" with the file path
 
-            percorso = percorso.substr(0, percorso.find(".", 0));
+            percorso = percorso.substr(0, percorso.find(".", 0));   //remove everyithing after "." included
 
             if (cartellaoutput.empty())
             {
@@ -69,13 +86,12 @@ int main()
                 z = percorso.find_last_of("\\");     
                 percorso = percorso.substr(z + 1);
                 percorso = cartellaoutput + "\\" + percorso;
-                cout << percorso << endl;
                 while (argomenti.find("perout") != string::npos)
                     argomenti.replace(argomenti.find("perout"), 6, percorso);
             }
            
-            cout << "start " + eseguibile + " " + argomenti << endl;      //it cout the result for debugging purposes, in the final version it will start the process
-            system(("start " + eseguibile + " " + argomenti).c_str());
+            cout << "start " + spazifix + eseguibile + " " + argomenti << endl;      //it cout the result for debugging purposes, in the final version it will start the process
+            system(("start " + spazifix + eseguibile + " " + argomenti).c_str());
             argomenti = argomentioriginale;
             attivi++;
 
